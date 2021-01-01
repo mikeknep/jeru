0e905e454853cf828c6b372c2f3f7dfa6158be06
Mike Knepper <michaelrknepper@gmail.com>
2021-01-01 16:45:42 -0600

    Define naive implementation for identifying possible refactors

--- lib/main.go
+++ lib/main.go
@@ -34,7 +34,41 @@ func (pr PossibleRefactor) AsCommand() string {
 }
 
 func (plan Plan) PossibleRefactors() []PossibleRefactor {
-	return []PossibleRefactor{}
+	var possibleRefactors []PossibleRefactor
+
+	var beingDeleted []ChangingResource
+	var beingCreated []ChangingResource
+
+	for _, cr := range plan.ChangingResources {
+		actions := cr.Change.Actions
+		switch {
+		case len(actions) == 2:
+			// "replace" actions are represented as either ["delete", "create"] or ["create", "delete"]
+			// These changes cannot be avoided with terraform state mv, and so are ignored here
+		case actions[0] == "delete":
+			beingDeleted = append(beingDeleted, cr)
+		case actions[0] == "create":
+			beingCreated = append(beingCreated, cr)
+		}
+	}
+
+	for _, deleting := range beingDeleted {
+		for _, creating := range beingCreated {
+			if isPossiblySameResource(deleting, creating) {
+				possibleRefactors = append(possibleRefactors, PossibleRefactor{
+					NewAddress: creating.Address,
+					OldAddress: deleting.Address,
+				})
+			}
+		}
+	}
+
+	return possibleRefactors
+}
+
+// this is not sophisticated enough yet
+func isPossiblySameResource(a, b ChangingResource) bool {
+	return a.Type == b.Type && a.ProviderName == b.ProviderName
 }
 
 func Terraform(args []string, stdout io.Writer) *exec.Cmd {
--- lib/main_test.go
+++ lib/main_test.go
@@ -24,6 +24,40 @@ func TestParsesPlanFile(t *testing.T) {
 	require.Equal(t, 2, len(plan.ChangingResources))
 }
 
+func TestIdentifiesASimplePossibleRefactorMatchingOnType(t *testing.T) {
+	plan := Plan{
+		ChangingResources: []ChangingResource{
+			ChangingResource{
+				Address:      "some_resource.old",
+				Change:       Change{Actions: []string{"delete"}},
+				Name:         "old",
+				ProviderName: "some_provider",
+				Type:         "some_resource",
+			},
+			ChangingResource{
+				Address:      "some_resource.new",
+				Change:       Change{Actions: []string{"create"}},
+				Name:         "new",
+				ProviderName: "some_provider",
+				Type:         "some_resource",
+			},
+			ChangingResource{
+				Address:      "completely_different.foo",
+				Change:       Change{Actions: []string{"create"}},
+				Name:         "foo",
+				ProviderName: "some_provider",
+				Type:         "completely_different",
+			},
+		},
+	}
+	expectedPossibleRefactor := PossibleRefactor{
+		OldAddress: "some_resource.old",
+		NewAddress: "some_resource.new",
+	}
+
+	require.Equal(t, expectedPossibleRefactor, plan.PossibleRefactors()[0])
+}
+
 func parsePlanFile(path string) Plan {
 	jsonFile, _ := os.Open(path)
 	bytes, _ := ioutil.ReadAll(jsonFile)
