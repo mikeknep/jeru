08de8db01acb7288c478e0d39de7df5c988e4db7
Mike Knepper <michaelrknepper@gmail.com>
2021-02-22 19:16:39 -0600

    Add two high-level rollback tests

--- lib/rollback_test.go
+++ lib/rollback_test.go
@@ -1,21 +1,44 @@
 package lib
 
 import (
+	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestRollbackCommand(t *testing.T) {
-	// TODO
-	require.Equal(t, 1, 1)
+var (
+	mv = "terraform state mv module.a module.b"
+	im = "terraform import resource.a id"
+
+	noPresent = func(string, []string) { return }
+	noRun     = func(string) error { return nil }
+)
+
+func TestRollbackWritesGeneratedRollbackLinesToScriptInReverseOrder(t *testing.T) {
+	changes := strings.NewReader(strings.Join([]string{mv, im}, "\n"))
+	var builder strings.Builder
+	script := Script{Name: "", W: &builder}
+
+	Rollback(changes, &script, noPresent, noRun)
+
+	expectedRollback := `#! /bin/bash
+terraform state rm resource.a
+terraform state mv module.b module.a`
+	require.Equal(t, expectedRollback, builder.String())
 }
 
-func present(captured []string) func(intro string, lines []string) {
-	return func(intro string, lines []string) {
-		captured = append(captured, intro)
-		for _, line := range lines {
-			captured = append(captured, line)
-		}
+func TestRollbackExecutesScriptUsingRunFunc(t *testing.T) {
+	script := Script{Name: "Rollback", W: ioutil.Discard} // this moves from ioutil to io in Go 1.16
+
+	executedScript := ""
+	spyRun := func(name string) error {
+		executedScript = name
+		return nil
 	}
+
+	Rollback(strings.NewReader(mv), &script, noPresent, spyRun)
+
+	require.Equal(t, "Rollback", executedScript)
 }
