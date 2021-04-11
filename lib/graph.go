68aaef5107eb1009da124b0b5044472b00ab27fa
Mike Knepper <michaelrknepper@gmail.com>
2021-04-11 15:33:38 -0500

    wip: graph-based approach to refactor combinations

---
+++ lib/graph.go
@@ -0,0 +1,95 @@
+package lib
+
+import "fmt"
+
+type Node struct {
+	Action string // "create" or "delete"
+	Type   string
+	Name   string
+}
+
+type Edge struct {
+	a *Node
+	b *Node
+}
+
+func (e Edge) String() string {
+	return fmt.Sprintf("<a: %s, b: %s>", *e.a, *e.b)
+}
+
+func (e Edge) isValid() bool {
+	sameType := e.a.Type == e.b.Type
+	differentActions := e.a.Action != e.b.Action
+
+	return sameType && differentActions
+}
+
+func isValidSet(set []Edge) bool {
+	var seenNodes []*Node
+	for _, edge := range set {
+		// a set of edges is only valid if all its component edges are valid
+		if !edge.isValid() {
+			return false
+		}
+
+		// a set of edges is only valid if the edges' nodes are unique
+		// aka, each node can only have zero or one edge
+		for _, seenNode := range seenNodes {
+			if seenNode == edge.a || seenNode == edge.b {
+				return false
+			}
+		}
+
+		seenNodes = append(seenNodes, edge.a)
+		seenNodes = append(seenNodes, edge.b)
+	}
+
+	return true
+}
+
+func validEdgeCombinationsFor(nodes []Node) [][]Edge {
+	var allSets [][]Edge
+
+	find(nodes, []Edge{}, &allSets)
+
+	return allSets
+}
+
+func find(nodes []Node, current []Edge, results *[][]Edge) {
+	if len(nodes) < 2 {
+		if isValidSet(current) {
+			*results = append(*results, current)
+		}
+		return
+	}
+
+	nodeA := nodes[0] // pluck the first node from the set of nodes
+
+	// ensure we don't change the original nodes. slices do not copy!
+	remNodes := make([]Node, len(nodes)-1)
+	copy(remNodes, nodes[1:])
+
+	for i := 0; i < len(remNodes); i++ {
+		nodeB := remNodes[i]               // pluck another node from the set of nodes...
+		edge := Edge{a: &nodeA, b: &nodeB} // ...and create an Edge
+
+		// add the Edge to the set we're currently building
+		current = append(current, edge)
+
+		// remove the plucked nodeB from remNodes
+		// by copying all nodes up to that node,
+		// and all nodes after that node,
+		// and stitching those two slices together
+		nextSetFirstPart := make([]Node, i)
+		nextSetSecondPart := make([]Node, len(remNodes)-i-1)
+		copy(nextSetFirstPart, remNodes[:i])
+		copy(nextSetSecondPart, remNodes[i+1:])
+		nextSet := append(nextSetFirstPart, nextSetSecondPart...)
+
+		// recursively find more edges
+		find(nextSet, current, results)
+
+		// clear out the current collection as we bubble up out of recursion
+		current = current[:len(current)-1]
+	}
+}
---
+++ lib/graph_test.go
@@ -0,0 +1,141 @@
+package lib
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	createBucket1 = Node{Action: "create", Type: "bucket", Name: "bucketOne"}
+	deleteBucket1 = Node{Action: "delete", Type: "bucket", Name: "bucketOne"}
+
+	createBucket2 = Node{Action: "create", Type: "bucket", Name: "bucketTwo"}
+	deleteBucket2 = Node{Action: "delete", Type: "bucket", Name: "bucketTwo"}
+
+	createDatabase1 = Node{Action: "create", Type: "database", Name: "dbOne"}
+	deleteDatabase1 = Node{Action: "delete", Type: "database", Name: "dbOne"}
+
+	createDatabase2 = Node{Action: "create", Type: "database", Name: "dbTwo"}
+	deleteDatabase2 = Node{Action: "delete", Type: "database", Name: "dbTwo"}
+
+	createInstance = Node{Action: "create", Type: "instance", Name: "instanceOne"}
+	deleteInstance = Node{Action: "delete", Type: "instance", Name: "instanceOne"}
+)
+
+func TestGraphs(t *testing.T) {
+	tests := map[string]struct {
+		nodes        []Node
+		expectedSets [][]Edge
+	}{
+		"Simplest case: a single pair": {
+			nodes: []Node{
+				createBucket1,
+				deleteBucket1,
+			},
+			expectedSets: [][]Edge{
+				[]Edge{
+					Edge{a: &createBucket1, b: &deleteBucket1},
+				},
+			},
+		},
+
+		"Two pairs of different types": {
+			nodes: []Node{
+				createBucket1,
+				deleteBucket1,
+				createDatabase1,
+				deleteDatabase1,
+			},
+			expectedSets: [][]Edge{
+				[]Edge{
+					Edge{a: &createBucket1, b: &deleteBucket1},
+					Edge{a: &createDatabase1, b: &deleteDatabase1},
+				},
+			},
+		},
+
+		"Two pairs of same type": {
+			nodes: []Node{
+				createBucket1,
+				deleteBucket1,
+				createBucket2,
+				deleteBucket2,
+			},
+			expectedSets: [][]Edge{
+				[]Edge{
+					// This set contains the "correct" pairs
+					Edge{a: &createBucket1, b: &deleteBucket1},
+					Edge{a: &createBucket2, b: &deleteBucket2},
+				},
+				[]Edge{
+					// This set is also *valid*
+					Edge{a: &createBucket1, b: &deleteBucket2},
+					Edge{a: &deleteBucket1, b: &createBucket2},
+				},
+			},
+		},
+
+		"Complex": {
+			nodes: []Node{
+				createBucket1,
+				deleteBucket1,
+				createBucket2,
+				deleteBucket2,
+				createDatabase1,
+				deleteDatabase1,
+				createDatabase2,
+				deleteDatabase2,
+				createInstance,
+				deleteInstance,
+			},
+			expectedSets: [][]Edge{
+				[]Edge{
+					// correct buckets, correct databases
+					Edge{a: &createBucket1, b: &deleteBucket1},
+					Edge{a: &createBucket2, b: &deleteBucket2},
+					Edge{a: &createDatabase1, b: &deleteDatabase1},
+					Edge{a: &createDatabase2, b: &deleteDatabase2},
+					Edge{a: &createInstance, b: &deleteInstance},
+				},
+				[]Edge{
+					// correct buckets, wrong databases
+					Edge{a: &createBucket1, b: &deleteBucket1},
+					Edge{a: &createBucket2, b: &deleteBucket2},
+					Edge{a: &createDatabase1, b: &deleteDatabase2},
+					Edge{a: &deleteDatabase1, b: &createDatabase2},
+					Edge{a: &createInstance, b: &deleteInstance},
+				},
+				[]Edge{
+					// wrong buckets, correct databases
+					Edge{a: &createBucket1, b: &deleteBucket2},
+					Edge{a: &deleteBucket1, b: &createBucket2},
+					Edge{a: &createDatabase1, b: &deleteDatabase1},
+					Edge{a: &createDatabase2, b: &deleteDatabase2},
+					Edge{a: &createInstance, b: &deleteInstance},
+				},
+				[]Edge{
+					// wrong buckets, wrong databases
+					Edge{a: &createBucket1, b: &deleteBucket2},
+					Edge{a: &deleteBucket1, b: &createBucket2},
+					Edge{a: &createDatabase1, b: &deleteDatabase2},
+					Edge{a: &deleteDatabase1, b: &createDatabase2},
+					Edge{a: &createInstance, b: &deleteInstance},
+				},
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			// first, sanity check that test setup is valid
+			for _, set := range tc.expectedSets {
+				require.True(t, isValidSet(set))
+			}
+
+			// then execute the test
+			computedSets := validEdgeCombinationsFor(tc.nodes)
+			require.Equal(t, tc.expectedSets, computedSets)
+		})
+	}
+}
